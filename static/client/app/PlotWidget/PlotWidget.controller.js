069cf3f6a561c71ebbcf53f46aa32c3ff2f40f42
Harald Held <harald.held@gmail.com>
2015-03-14 10:42:35 +0100

    Basic plotting works now.

--- static/client/app/PlotWidget/PlotWidget.controller.js
+++ static/client/app/PlotWidget/PlotWidget.controller.js
@@ -12,11 +12,31 @@
     function PlotWidgetController() {
         var vm = this;
 
-        vm.data = [];
-        vm.options = [];
+        vm.data = data();
+        vm.options = options();
         vm.width = 800;
         vm.height = 400;
 
         // ####################################################################
+
+        function data() {
+            return [
+                {x: 0, value: 12},
+                {x: 1, value: 0},
+                {x: 2, value: 23}
+            ];
+        }
+
+        function options() {
+            return {
+                tooltipMode: 'scrubber',
+                lineMode: 'cardinal',
+                tension: 0.3,
+                series: [
+                    {y: 'value', type: 'area'},
+                    {y: 'x', type: 'area'}
+                ]
+            };
+        }
     }
 }());
