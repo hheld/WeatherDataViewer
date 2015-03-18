4942ff7969f66996915da5cd1b3f027ae3b17353
Harald Held <harald.held@gmail.com>
2015-03-18 21:37:35 +0100

    Better organization of gulp file. Now you can do the watch jobs if desired.

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,23 @@ var gulp    = require('gulp'),
 
 gulp.task('brackets-onsave', ['uglify', 'distributeHtml', 'distributeCss']);
 
-gulp.watch('static/client/**/*.js', ['uglify']);
+gulp.task('watch', function() {
+    gulp.watch('static/client/**/*.js', ['uglify']);
+    gulp.watch('static/client/**/*.html', ['distributeHtml']);
+    gulp.watch('static/client/**/*.css', ['distributeCss']);
+});
+
 gulp.task('uglify', function () {
     gulp.src(['static/client/**/*.js'])
         .pipe(uglify('WeatherData.min.js'))
         .pipe(gulp.dest('static/dist'));
 });
 
-gulp.watch('static/client/**/*.html', ['distributeHtml']);
 gulp.task('distributeHtml', function () {
     gulp.src('static/client/**/*.html')
         .pipe(gulp.dest('static/dist'));
 });
 
-gulp.watch('static/client/**/*.css', ['distributeCss']);
 gulp.task('distributeCss', function () {
     gulp.src('static/client/**/*.css')
         .pipe(gulp.dest('static/dist'));
