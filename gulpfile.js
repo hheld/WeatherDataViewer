2faec2307705c3b0f0083fd283b2bb16867afe96
Harald Held <harald.held@gmail.com>
2015-03-19 15:31:06 +0100

    Adapted gulpfile.js such that errors don't lead to termination of the watch process.

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ gulp.task('watch', function() {
 gulp.task('uglify', function () {
     gulp.src(['static/client/**/*.js'])
         .pipe(uglify('WeatherData.min.js'))
+        .on('error', swallowError)
         .pipe(gulp.dest('static/dist'));
 });
 
@@ -28,3 +29,8 @@ gulp.task('distributeCss', function () {
 });
 
 gulp.task('default', ['uglify', 'distributeHtml', 'distributeCss']);
+
+function swallowError(error) {
+    console.log(error.toString());
+    this.emit('end');
+}
